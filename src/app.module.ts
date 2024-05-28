9045492cd331c9966d7df4645f7946e0a35c92a8
Hugo Virgen <virgenherrera@gmail.com>
2024-05-28 13:49:48 -0600

    init module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
-  imports: [],
+  imports: [ContactModule],
   controllers: [AppController],
   providers: [AppService],
 })
